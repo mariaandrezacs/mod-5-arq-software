fd7c9884e6d8c489b9164183f8589c485d0a6c67
Maria Andreza C Santos <mandreza008@gmail.com>
2025-02-01 17:42:42 -0300

    config: settings project pylint

---
+++ ex_pylint.py
@@ -0,0 +1,11 @@
+''' Alguma documentação sobre o arquivo... '''
+
+print("Ola mundo")
+
+def minha_funcao():
+    ''' alguma coisa '''
+    print("Estou na minha função")
+
+# snake_case -> Variaveis e Funções: my_variable
+# cammelCase -> Não consta: minhaFunção xxxxx
+# PascalCase -> Classes em Python
