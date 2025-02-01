0060a6c8a34cfd6e1e80a67c88c436a678bd6415
Maria Andreza C Santos <mandreza008@gmail.com>
2025-02-01 17:54:14 -0300

    config: adding pylint to project

--- ex_pylint.py
+++ ex_pylint.py
@@ -1,11 +1,10 @@
-''' Alguma documentação sobre o arquivo... '''
-
 print("Ola mundo")
 
 def minha_funcao():
-    ''' alguma coisa '''
     print("Estou na minha função")
 
 # snake_case -> Variaveis e Funções: my_variable
 # cammelCase -> Não consta: minhaFunção xxxxx
 # PascalCase -> Classes em Python
+
+# pylint --generate-rcfile | out-file -encoding utf8 .pylintrc | codigo executar o .pylint
