8be35a30c29ba83590dade170e0a9992bfbbb788
mariaandrezacs <mandreza008@gmail.com>
2025-02-04 14:51:32 -0300

    fix: fixes db connection bug

--- src/models/sql/settings/connection.py
+++ src/models/sql/settings/connection.py
@@ -7,7 +7,7 @@ class DBConnectionHandler:
         self.__engine = None
 
     def connect_to_db(self):
-        self.__engine = create_engine
+        self.__engine = create_engine(self.__connection_string)
 
     def get_engine(self):
         return self.__engine
