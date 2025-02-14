e5e69b7889cf9f9f0eedbe681938fef030d3974c
mariaandrezacs <mandreza008@gmail.com>
2025-02-14 09:30:19 -0300

    test: realiza testes unitarios com dados de pets

--- src/models/sqlite/repositories/pets_repository_test.py
+++ src/models/sqlite/repositories/pets_repository_test.py
@@ -18,16 +18,17 @@ class MockConnection:
             ]
         )
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+    def __enter__(self): return self
+    def __exit__(self, exc_type, exc_val, exc_tb): pass
 
 
 def test_list_pets():
     mock_connection = MockConnection()
     repo = PetsRepository(mock_connection)
     response = repo.list_pets()
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.all.assert_called_once()
+    mock_connection.session.filter.assert_not_called()
 
     assert response[0].name == "dog"
