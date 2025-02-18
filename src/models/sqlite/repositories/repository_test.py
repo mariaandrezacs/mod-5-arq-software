ab08d73cb9d17589a1d8ca546078eb35ce2b856f
Maria Andreza C Santos <mandreza008@gmail.com>
2025-02-17 21:19:45 -0300

    config: add testes de pessoas

--- src/models/sqlite/repositories/repository_test.py
+++ src/models/sqlite/repositories/repository_test.py
@@ -1,8 +1,9 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
-# db_connection_handler.connect_to_db()
+db_connection_handler.connect_to_db()
 
 
 @pytest.mark.skip(reason="Interação com o banco")
@@ -17,3 +18,13 @@ def test_delete_pet():
     name = "belinha"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="Interação com o banco")
+def test_insert_person():
+    first_name = "test name"
+    last_name = "test last"
+    age = 77
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
