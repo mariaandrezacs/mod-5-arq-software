697fab6c2820e8e636ecdf2d53a961265f7ce411
Maria Andreza C Santos <mandreza008@gmail.com>
2025-03-08 19:24:20 -0300

    feat: adiciona inicio de controllers

---
+++ src/controllers/person_creator_controller.py
@@ -0,0 +1,8 @@
+from ..models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
+
+
+class PersonCreatorController:
+    def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
+        # self.__people_repository = people_repository
+        # self.__people_repository.get_person
+        pass
