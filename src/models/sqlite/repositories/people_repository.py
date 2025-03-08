cc18d60382ee561269b723b440054fc0cb44e436
Maria Andreza C Santos <mandreza008@gmail.com>
2025-03-08 19:20:58 -0300

    config: atualiza arquivos com interfaces

--- src/models/sqlite/repositories/people_repository.py
+++ src/models/sqlite/repositories/people_repository.py
@@ -1,20 +1,21 @@
 from sqlalchemy.orm.exc import NoResultFound
+
 from ..entities.people import PeopleTable
 from ..entities.pets import PetsTable
+from ..interfaces.people_repository import PeopleRepositoryInterface
 
 
-class PeopleRepository:
+class PeopleRepository(PeopleRepositoryInterface):
     def __init__(self, db_connection) -> None:
         self.__db_connection = db_connection
 
-    def insert_person(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
+    def insert_person(
+        self, first_name: str, last_name: str, age: int, pet_id: int
+    ) -> None:
         with self.__db_connection as database:
             try:
                 person_data = PeopleTable(
-                    first_name=first_name,
-                    last_name=last_name,
-                    age=age,
-                    pet_id=pet_id
+                    first_name=first_name, last_name=last_name, age=age, pet_id=pet_id
                 )
                 database.session.add(person_data)
                 database.session.commit()
@@ -26,15 +27,14 @@ class PeopleRepository:
         with self.__db_connection as database:
             try:
                 person = (
-                    database.session
-                    .query(PeopleTable)
+                    database.session.query(PeopleTable)
                     .join(PetsTable, PetsTable.id == PeopleTable.pet_id)
                     .filter(PeopleTable.id == person_id)
                     .with_entities(
                         PeopleTable.first_name,
                         PeopleTable.last_name,
                         PetsTable.name.label("pet_name"),
-                        PetsTable.type.label("pet_type")
+                        PetsTable.type.label("pet_type"),
                     )
                     .one()
                 )
