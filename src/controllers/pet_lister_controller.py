e8fc3ed3f7f343b143d8d2acc5ed8748f4bb69e6
Maria Andreza C Santos <mandreza008@gmail.com>
2025-03-10 22:38:38 -0300

    feat: adiciona listador de pets

---
+++ src/controllers/pet_lister_controller.py
@@ -0,0 +1,30 @@
+from src.models.sqlite.entities.pets import PetsTable
+
+from ..models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
+
+
+class PetListerController:
+    def __init__(self, pet_repository: PetsRepositoryInterface) -> None:
+        self.__pet_repository = pet_repository
+
+    def lister(self) -> dict:
+        pets = self.__get_pets_in_db()
+        response = self.__format_response(pets)
+        return response
+
+    def __get_pets_in_db(self) -> list[PetsTable]:
+        pets = self.__pet_repository.list_pets()
+        return pets
+
+    def __format_response(self, pets: list[PetsTable]) -> dict:
+        formatted_pets = []
+        for pet in pets:
+            formatted_pets.append({"name": pet.name, "type": pet.type, "id": pet.id})
+
+        return {
+            "data": {
+                "type": "Pets",
+                "count": len(formatted_pets),
+                "attributes": formatted_pets,
+            }
+        }
