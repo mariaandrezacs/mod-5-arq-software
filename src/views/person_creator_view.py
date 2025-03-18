0af3f368d082800a9d85531b8d36d5d4d3d730b5
Maria Andreza C Santos <mandreza008@gmail.com>
2025-03-17 22:05:22 -0300

    feat: implementa validação na view de criação de pessoa

--- src/views/person_creator_view.py
+++ src/views/person_creator_view.py
@@ -1,6 +1,7 @@
 from src.controllers.interfaces.person_creator_controller import (
     PersonCreatorControllerInterface,
 )
+from src.validators.person_creator_validator import person_creator_validator
 
 from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
@@ -12,6 +13,7 @@ class PersonCreatorView(ViewInterface):
         self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
+        person_creator_validator(http_request)
         person_info = http_request.body
         body_response = self.__controller.create(person_info)
         return HttpResponse(status_code=201, body=body_response)
