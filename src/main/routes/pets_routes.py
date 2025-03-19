deb58912e8ae0efd39f0c127e4b3f3babcfb031c
Maria Andreza C Santos <mandreza008@gmail.com>
2025-03-18 22:16:27 -0300

    feat: adiciona handler errors in routes

--- src/main/routes/pets_routes.py
+++ src/main/routes/pets_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify
 from src.main.composer.pet_delete_composer import pet_delete_composer
 from src.main.composer.pet_lister_composer import pet_lister_composer
 
+from ...errors.error_handler import handle_errors
 from ...views.http_types.http_request import HttpRequest
 
 pet_route_bp = Blueprint("pets_routes", __name__)
@@ -10,17 +11,26 @@ pet_route_bp = Blueprint("pets_routes", __name__)
 
 @pet_route_bp.route("/pets", methods=["GET"])
 def list_pets():
-    http_request = HttpRequest()
-    view = pet_lister_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest()
+        view = pet_lister_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 
 @pet_route_bp.route("/pets/<name>", methods=["DELETE"])
 def delete_pet(name):
-    http_request = HttpRequest(param={"name": name})
-    view = pet_delete_composer()
-    http_reponse = view.handle(http_request)
+    try:
+        http_request = HttpRequest(param={"name": name})
+        view = pet_delete_composer()
+        http_reponse = view.handle(http_request)
 
-    return jsonify(http_reponse.body), http_reponse.status_code
+        return jsonify(http_reponse.body), http_reponse.status_code
+
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_reponse.body), http_response.status_code
